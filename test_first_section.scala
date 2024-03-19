cc4da15b7d60dafb4d1543621206eea2d867c310
Brandon Kwamou <brandonkwamou@gmail.com>
2024-03-19 06:00:23 +0100

    Scala Programming Level One

---
+++ test_first_section.scala
@@ -0,0 +1,19 @@
+// What is 2 to the power of 5?
+Math.pow(2,5) // 32.0
+
+// What is the remainder of 180 divided by 7 ?
+180 % 7 // 5
+
+// Given variable pet_name = "Sammy", use string interpolation to print out "My dog's name is Sammy."
+f"My dog's name is $pet_name"
+
+// Use scala to find out if the letter sequence "xyz" is contained in : "sadfjshfjyuyxyzjkfuidkjklhasyysdfk"
+"sadfjshfjyuyxyzjkfuidkjklhasyysdfk.contains".contains("xyz")
+
+// What is the difference between a value and a variable
+a value cannot be reassigned (constance)
+and a variable can be reassigned to the same datatype (let)
+
+// Given the tuple (1,2,3,(4,5,6)) retrieve the number 6
+var my_tup = (1,2,3,(4,5,6))
+my_tup._4._3
